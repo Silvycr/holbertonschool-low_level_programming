293a022cddddde6bc94c02f5cdf8be66bf901f71
Silvycr <silvia_alinacr@live.com>
2023-06-21 12:03:49 -0700

    Program that prints all possible combinations of two

---
+++ variables_if_else_while/102-print_comb5.c
@@ -0,0 +1,35 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+/**
+ * main - Prints all possible combinations of two
+ *
+ * Return: zero on success
+ */
+int main(void)
+{
+	int sa;
+	int cr;
+
+	for (sa = 0; sa <= 99; sa++)
+	{
+		for (cr = sa + 1; cr <= 99; cr++)
+		{
+			putchar((sa / 10) + '0');
+			putchar((sa % 10) + '0');
+			putchar(' ');
+			putchar((cr / 10) + '0');
+			putchar((cr % 10) + '0');
+
+			if (sa == 99 && cr == 99) /*A conditional*/
+				continue;
+
+			putchar(',');
+			putchar(' ');
+		}
+	}
+
+	putchar('\n');
+
+	return (0);
+}
