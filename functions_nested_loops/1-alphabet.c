87c69caf8e42d6ce47c9dc6e1b84cc4543a9d47e
Silvycr <silvia_alinacr@live.com>
2023-06-21 14:22:12 -0700

    Prints the alphabet, in lowercase

---
+++ functions_nested_loops/1-alphabet.c
@@ -0,0 +1,16 @@
+#include "main.h"
+/**
+ * main - Prints the alphabet, in lowercase
+ *
+ * Return: Always 0.
+ */
+void print_alphabet(void)
+{
+	char al;
+
+	for (al = 'a'; al <= 'z'; al++)
+	{
+		_putchar(al);
+	}
+	_putchar('\n');
+}
