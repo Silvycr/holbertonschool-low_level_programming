7a7544a71dbc625bfd2f41ca1086df60797955f5
Silvycr <silvia_alinacr@live.com>
2023-06-21 13:31:09 -0700

    Putchar

---
+++ functions_nested_loops/0-putchar.c
@@ -0,0 +1,20 @@
+#include <unistd.h>
+#include "main.h"
+/**
+ * main - Prints _putchar
+ *
+ * Return: zero on success
+ */
+int main(void)
+{
+	char msg[8] = "_putchar"
+	int al;
+
+	for (al = 0; al <= 9; al++)
+	{
+		_putchar(msg[al]);
+	}
+	_putchar('\n');
+
+	return (0);
+}
