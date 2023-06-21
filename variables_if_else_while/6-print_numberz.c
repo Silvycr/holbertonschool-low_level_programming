d659b513944c2a6f8822c81ce27cc51eba21e444
Silvycr <silvia_alinacr@live.com>
2023-06-20 18:02:58 -0700

    NumberZ

---
+++ variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+#include <stdlib.h>
+/**
+ * main - Numberz
+ *
+ * Return: zero on success
+ */
+int main(void)
+{
+	int co;
+
+	for (co = '0'; co <= '9'; co++)
+	{
+		putchar(co);
+	}
+	putchar('\n');
+
+	return (0);
+}
