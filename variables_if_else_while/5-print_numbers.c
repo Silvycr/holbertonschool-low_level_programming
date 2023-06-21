3341ce5f2c06e04edd879ae5d4ec7a157091ab73
Silvycr <silvia_alinacr@live.com>
2023-06-20 17:49:55 -0700

    Numbers

---
+++ variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+/**
+ * main - Number
+ *
+ * Return: zero on success
+ */
+int main(void)
+{
+	int al;
+
+	for (al = 0; al < 10; al++)
+		printf("%d", al);
+	printf("\n");
+
+	return (0);
+}
