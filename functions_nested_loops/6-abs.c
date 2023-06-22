926a547a7be6ed25c895f9bdf447a01e7fcef38f
Silvycr <silvia_alinacr@live.com>
2023-06-22 06:30:27 -0700

    Function that computes the absolute value

---
+++ functions_nested_loops/6-abs.c
@@ -0,0 +1,17 @@
+#include "main.h"
+/**
+ * _abs - Function that computes the absolute value
+ * @al: Input a number
+ * Return: Absolute value
+ **/
+int _abs(int al)
+{
+	if (al >= 0)
+	{
+		return (al);
+	}
+	else
+	{
+		return (al * -1);
+	}
+}
