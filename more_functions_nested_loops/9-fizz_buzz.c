eae92cadcfa6bcbc0a5d42840739c34a0ba92b32
Silvycr <silvia_alinacr@live.com>
2023-06-24 11:10:10 -0700

    Program that prints the numbers with Fizz-buzz

--- more_functions_nested_loops/9-fizz_buzz.c
+++ more_functions_nested_loops/9-fizz_buzz.c
@@ -11,21 +11,13 @@ int main(void)
 	for (z = 1; z <= 100; z++)
 	{
 		if (z % 3 == 0 && z % 5 == 0)
-		{
 			printf("FizzBuzz ");
-		}
 		else if (z % 3 == 0)
-		{
 			printf("Fizz ");
-		}
 		else if (z % 5 == 0)
-		{
 			printf("Buzz ");
-		}
 		else
-		{
 			printf("%d ", z);
-		}
 	}
 	printf("\n");
 
