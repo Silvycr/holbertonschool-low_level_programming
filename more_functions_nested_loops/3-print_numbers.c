dcfab48e365aff7472cf1a37ca5c780e343a5bd5
Silvycr <silvia_alinacr@live.com>
2023-06-24 08:45:33 -0700

    Function that prints the numbers

---
+++ more_functions_nested_loops/3-print_numbers.c
@@ -0,0 +1,15 @@
+#include "main.h"
+/**
+ * print_numbers - *Function that prints the numbers
+ * Return: none
+ **/
+void print_numbers(void)
+{
+	int sa;
+
+	for (sa = 48; sa < 58; sa++)
+	{
+		_putchar(sa);
+	}
+	_putchar('\n');
+}
