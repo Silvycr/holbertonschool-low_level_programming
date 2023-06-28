45ff8dfdbe5c788b26c0002d7101ae3018a482c7
Silvycr <silvia_alinacr@live.com>
2023-06-27 22:02:14 -0700

    Function that returns the natural square

---
+++ recursion/5-sqrt_recursion.c
@@ -0,0 +1,24 @@
+#include "main.h"
+/**
+ * test_root - Tests for maximum squares
+ * @n: Number being evaluated
+ * @i: Test values being iterated
+ * Return: Square root
+ **/
+int test_root(int n, int i)
+{
+	if (i * i == n)
+		return (i);
+	else if (i * i > n)
+		return (-1);
+	return (test_root(n, i + 1));
+}
+/**
+ * _sqrt_recursion - Returns natural square
+ * @n: Number being evaluated
+ * Return: Square root
+ **/
+int _sqrt_recursion(int n)
+{
+	return (test_root(n, 1));
+}
