3cfcaedda25d13a8d9e5f51b3e8b33461549f4e8
Silvycr <silvia_alinacr@live.com>
2023-07-05 15:31:01 -0700

    Update

--- variadic_functions/variadic_functions.h
+++ variadic_functions/variadic_functions.h
@@ -1,6 +1,12 @@
-#ifndef _variadic_functions_h
-#define _variadic_functions_h
+#ifndef _variadic_functions_h_
+#define _variadic_functions_h_
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <stdarg.h>
+
 int _putchar(char c);
 int sum_them_all(const unsigned int n, ...);
+void print_numbers(const char *separator, const unsigned int n, ...);
 
 #endif
