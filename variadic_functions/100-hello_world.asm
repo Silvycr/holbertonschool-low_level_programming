45860f7a50895b7a54130a9880808588e8905348
Silvycr <silvia_alinacr@live.com>
2023-07-09 15:55:38 -0700

    64-bit program in assembly

--- variadic_functions/100-hello_world.asm
+++ variadic_functions/100-hello_world.asm
@@ -1,17 +1,16 @@
-	global main
+section .data
+	msg db "Hello, World", 10
 
 	section .text
+	global main
 
 main:
 	mov rax, 1
 	mov rdi, 1
-	mov rsi, message
+	mov rsi, msg
 	mov rdx, 17
 	syscall
 
 	mov rax, 60
 	mov rdi, 0
 	syscall
-
-message:
-	db "Hello, World", 10
