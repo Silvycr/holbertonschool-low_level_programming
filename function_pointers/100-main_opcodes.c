f84a36f0637d9e93ff305d43e3203ad158d8acd6
Silvycr <silvia_alinacr@live.com>
2023-07-09 15:41:35 -0700

    Program that prints the opcodes

---
+++ function_pointers/100-main_opcodes.c
@@ -0,0 +1,37 @@
+#include <stdio.h>
+#include <stdlib.h>
+/**
+ * main - check the code.
+ * @argc: argument count.
+ * @argv: argument vector.
+ * Return:Zero Always.
+ */
+int main(int argc, char *argv[])
+{
+	char *opc = (char *) main;
+	int x, nbytes;
+
+	if (argc != 2)
+	{
+		printf("Error\n");
+		exit(1);
+	}
+
+	nbytes = atoi(argv[1]);
+
+	if (nbytes < 0)
+	{
+		printf("Error\n");
+		exit(2);
+	}
+
+	for (x = 0; x < nbytes; x++)
+	{
+		printf("%02x", opc[x] & 0xFF);
+		if (x != nbytes - 1)
+			printf(" ");
+	}
+
+	printf("\n");
+	return (0);
+}
